3e8f2937905a65fcf43f0143f695abb474dcec28
La <noahsmatrix@gmail.com>
2019-03-07 12:49:24 -0600

    Added basic code for home page

--- wcag_checker/home/models.py
+++ wcag_checker/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 class checkRequest(models.Model):
     url = models.CharField(max_length=700)
@@ -8,4 +9,9 @@ class checkRequest(models.Model):
 
     def getForm():
         # This function is meant to return the form to be rendered for this model.
+        
         return ""
+
+
+class URLForm(forms.Form):
+    url = forms.CharField(label='Website to check', max_length=3000)
--- wcag_checker/home/views.py
+++ wcag_checker/home/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 
-from .models import checkRequest
-
+from .models import checkRequest, URLForm
+from utils.compliance_checker import checkSite
 
 def requestView(request):
 
     context = {}
-    
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+
+        # create a form instance and populate it with data from the request:
+        form = URLForm(request.POST)
+        
+        # check whether it's valid:
+        if form.is_valid():
+            print('Valid form')
+            websiteURL = form.cleaned_data['url']
+
+            print(websiteURL)
+            siteStanding = checkSite(websiteURL)
+            print(siteStanding)
+
+            context['form'] = URLForm() 
+            return render(request, 'home.html', context)
+
+    else:
+        context['form'] = URLForm() 
+        
     return render(request, 'home.html', context)
